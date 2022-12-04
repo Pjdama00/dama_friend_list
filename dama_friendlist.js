5e3b51acf25742782a1b7ba687342130a7eb5087
Pjdama00 <patrickjohn.dama-20@pu.edu.ph>
2022-12-04 17:45:51 +0800

    dama

---
+++ dama_friendlist.js
@@ -0,0 +1,39 @@
+[
+    {
+        "First_Name" : "Patrick John  ",
+        "Last_Name" : "Dama",
+        "Age" : "22",
+        "Address" : "Bacolod City",
+        "Contact_Number" : "09388808725"
+    },
+  
+    {
+        "Friends List" : [
+          {
+              "Name": "Kristin",
+              "Age": "21",
+              "Address": "Bacolod City",
+              "Likes" :"Volleyball",
+              "Dislikes" : "Math Subject", 
+              "Feeling" : "Tired"
+          },
+          {
+            "Name": "Ella Mae",
+            "Age": "23",
+            "Address": "Bacolod City",
+            "Likes" :"SINGING",
+            "Dislikes" : "Filipino", 
+            "Feeling" : "Tired"
+        },
+          {
+            "Name": "Mitchell",
+            "Age": "25",
+            "Address": "Bacolod City",
+            "Likes" :"Playing Online Games ",
+            "Dislikes" : "Jogging", 
+            "Feeling" : "Happy"
+          },
+      ]
+      }
+ 
+]
